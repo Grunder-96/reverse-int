40a228c97c982e82dd85b2a3636ccf70e14d04f9
Grigory Taran <grunder-96@mail.ru>
2021-03-07 19:06:46 +0300

    Complete.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let m = 0;
+    let string = String(Math.abs(n));
+    for (let i = 0; i < string.length; i++) {
+        m = m + string[i] * Math.pow(10, i);
+    }
+    return m;
 }
+
